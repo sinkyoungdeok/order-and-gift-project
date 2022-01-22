0bed2fa277f611e6f840ddb299a68d0dc1c49833
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-22 15:54:19 +0900

    [UPDATE] hellocontroller 예시 변경

--- order/src/main/kotlin/msa/order/HelloController.kt
+++ order/src/main/kotlin/msa/order/HelloController.kt
@@ -3,13 +3,14 @@ package msa.order
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Flux
 
 @RestController
 @RequestMapping("/")
 class HelloController {
 
     @GetMapping
-    fun hello(): String {
-        return "hello";
+    fun hello(): Flux<String> {
+        return Flux.just("hello")
     }
 }
