7600ad8d6c2523462094d2a3d04b908ca9a0c4c1
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-22 15:13:40 +0900

    [UPDATE] (partner) PartnerInfo null처리하면서 잘 반환되게 변경

--- order/src/main/kotlin/msa/order/domain/PartnerInfo.kt
+++ order/src/main/kotlin/msa/order/domain/PartnerInfo.kt
@@ -9,11 +9,11 @@ data class PartnerInfo(
     var status: Partner.Status
 ) {
     constructor(partner: Partner) : this(
-        "",
-        "",
-        "",
-        "",
-        "",
-        Partner.Status.ENABLE
+        partner.id ?: "",
+        partner.partnerToken ?: "",
+        partner.partnerName ?: "",
+        partner.businessNo ?: "",
+        partner.email ?: "",
+        partner.status ?: Partner.Status.ENABLE
     )
 }
