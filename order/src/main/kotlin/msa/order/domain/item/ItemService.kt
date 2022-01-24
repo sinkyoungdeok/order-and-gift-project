e5c687889d4a489f5f3739159e8b45eb9856f14e
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-24 20:08:25 +0900

    [UPDATE] (item) 코드 정리

--- order/src/main/kotlin/msa/order/domain/item/ItemService.kt
+++ order/src/main/kotlin/msa/order/domain/item/ItemService.kt
@@ -3,5 +3,8 @@ package msa.order.domain.item
 import reactor.core.publisher.Mono
 
 interface ItemService {
-    fun registerItem(request: Mono<ItemCommand.RegisterItemRequest>, partnerToken: Mono<String>): Mono<ItemInfo.Token>
+    fun registerItem(
+        request: Mono<ItemCommand.RegisterItemRequest>,
+        partnerToken: Mono<String>
+    ): Mono<ItemInfo.Token>
 }
