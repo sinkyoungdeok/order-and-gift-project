fb1e11b1c5d755012726ad495fe3b95027102690
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-24 21:45:08 +0900

    [ADD] (Partner) 파트너 조회 기능

--- order/src/main/kotlin/msa/order/infrastructure/partner/PartnerRepository.kt
+++ order/src/main/kotlin/msa/order/infrastructure/partner/PartnerRepository.kt
@@ -3,6 +3,9 @@ package msa.order.infrastructure.partner
 import msa.order.domain.partner.Partner
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Mono
 
 @Repository
-interface PartnerRepository : ReactiveMongoRepository<Partner, String>
+interface PartnerRepository : ReactiveMongoRepository<Partner, String> {
+    fun findByPartnerName(partnerToken: String): Mono<Partner>
+}
