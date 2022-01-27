30a875a0846b9d1b43090e2c3885fbab9c0fab97
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-27 22:31:09 +0900

    [ADD] (Item) ItemOption, ItemOptionGroup 도메인 entity 추가 작업

--- order/src/main/kotlin/msa/order/domain/item/optiongroup/ItemOptionGroup.kt
+++ order/src/main/kotlin/msa/order/domain/item/optiongroup/ItemOptionGroup.kt
@@ -2,11 +2,13 @@ package msa.order.domain.item.optiongroup
 
 import msa.order.domain.AbstractEntity
 import msa.order.domain.item.option.ItemOption
+import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document
 data class ItemOptionGroup(
-    val id: Long? = null,
+    @Id val id: String? = null,
+    val ordering: Int? = null,
     val itemOptionGroupName: String? = null,
     val itemOptionList: List<ItemOption>? = null,
 ) : AbstractEntity() {
