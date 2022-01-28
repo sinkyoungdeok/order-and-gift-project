91240364f2354120b0377646ee9b1d08480a6b61
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-28 16:58:36 +0900

    [UPDATE] (partner) 파트너 저장 API 랜덤 token값 저장되게 변경

--- order/src/main/kotlin/msa/order/domain/partner/PartnerCommand.kt
+++ order/src/main/kotlin/msa/order/domain/partner/PartnerCommand.kt
@@ -1,5 +1,7 @@
 package msa.order.domain.partner
 
+import msa.order.common.util.TokenGenerator
+
 class PartnerCommand {
 
     data class RegisterPartner(
@@ -8,5 +10,16 @@ class PartnerCommand {
         var email: String
     ) {
         constructor() : this("", "", "")
+
+        private val PARTNER_PREFIX = "ptn_"
+
+        fun toEntity(): Partner {
+            return Partner(
+                partnerToken = TokenGenerator.randomCharacterWithPrefix(PARTNER_PREFIX),
+                partnerName = partnerName,
+                businessNo = businessNo,
+                email = email,
+            )
+        }
     }
 }
