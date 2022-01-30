9176bee5677f07d7e6e9e2e29b51bc3d8771085c
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-30 14:50:28 +0900

    [ADD] (Order) Order 주문 결재 API - infra 작업 완료

--- order/src/main/kotlin/msa/order/infrastructure/order/payment/PaymentProcessorImpl.kt
+++ order/src/main/kotlin/msa/order/infrastructure/order/payment/PaymentProcessorImpl.kt
@@ -1,15 +1,28 @@
 package msa.order.infrastructure.order.payment
 
+import msa.order.common.exception.InvalidParamException
 import msa.order.domain.order.Order
 import msa.order.domain.order.OrderCommand
 import msa.order.domain.order.payment.PaymentProcessor
+import msa.order.domain.order.payment.validator.PaymentValidator
 import org.springframework.stereotype.Component
+import java.util.function.Supplier
 
 @Component
 class PaymentProcessorImpl(
+    val paymentValidatorList: List<PaymentValidator>,
     val paymentApiCallerList: List<PaymentApiCaller>
 ) : PaymentProcessor {
     override fun pay(order: Order, command: OrderCommand.PaymentRequest) {
-        TODO("Not yet implemented")
+        paymentValidatorList.forEach { it.validate(order, command) }
+        val payApiCaller = routingApiCaller(command)
+        payApiCaller.pay(command)
+    }
+
+    fun routingApiCaller(command: OrderCommand.PaymentRequest): PaymentApiCaller {
+        return paymentApiCallerList.stream()
+            .filter { it.support(command.payMethod) }
+            .findFirst()
+            .orElseThrow(Supplier<RuntimeException> { InvalidParamException() })
     }
 }
