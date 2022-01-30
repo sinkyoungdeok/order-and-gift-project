8843a264187a4f2beaff9bf9808b8b315391b87f
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-31 01:46:38 +0900

    [ADD] (Partner) 저장시 예외처리

--- order/src/main/kotlin/msa/order/infrastructure/partner/PartnerStoreImpl.kt
+++ order/src/main/kotlin/msa/order/infrastructure/partner/PartnerStoreImpl.kt
@@ -1,8 +1,10 @@
 package msa.order.infrastructure.partner
 
 import kotlinx.coroutines.reactor.awaitSingle
+import msa.order.common.exception.InvalidParamException
 import msa.order.domain.partner.Partner
 import msa.order.domain.partner.PartnerStore
+import org.apache.commons.lang3.StringUtils
 import org.springframework.stereotype.Component
 
 @Component
@@ -10,6 +12,11 @@ class PartnerStoreImpl(
     val partnerRepository: PartnerRepository
 ) : PartnerStore {
     override suspend fun store(initPartner: Partner): Partner {
+        if (StringUtils.isEmpty(initPartner.partnerToken)) throw InvalidParamException("param.partnerToken")
+        if (StringUtils.isEmpty(initPartner.partnerName)) throw InvalidParamException("partner.partnerName")
+        if (StringUtils.isEmpty(initPartner.businessNo)) throw InvalidParamException("partner.businessNo")
+        if (StringUtils.isEmpty(initPartner.email)) throw InvalidParamException("partner.email")
+
         return partnerRepository.save(initPartner).awaitSingle()
     }
 }
