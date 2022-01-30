099ee521031b763bdc7a11aab22d0671d495a422
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-30 19:50:17 +0900

    [ADD] (Item) 아이템 등록 후 이메일 알림 서비스 연동

---
+++ order/src/main/kotlin/msa/order/application/item/event/ItemEmailEvent.kt
@@ -0,0 +1,4 @@
+package msa.order.application.item.event
+
+class ItemEmailEvent {
+}
---
+++ order/src/main/kotlin/msa/order/application/item/listener/ItemEventListener.kt
@@ -0,0 +1,21 @@
+package msa.order.application.item.listener
+
+import msa.order.application.item.event.ItemEmailEvent
+import msa.order.application.partner.event.PartnerEmailEvent
+import msa.order.domain.notification.NotificationService
+import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Component
+
+@Component
+class ItemEventListener(
+    val notificationService: NotificationService
+) {
+
+    @EventListener
+    @Async("notificationEventHandler")
+    fun handleEmail(event: ItemEmailEvent) {
+        Thread.sleep(5000L) // 이메일 발송하는데 걸리는 시간
+        notificationService.sendEmail("", "title", "description")
+    }
+}
