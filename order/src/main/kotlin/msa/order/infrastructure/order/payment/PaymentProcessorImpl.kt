1a95aa25b5e7e4dc89b78184834f99ad3a09f391
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-30 14:21:55 +0900

    [ADD] (Order) Order 주문 결재 API - 인프라스트럭처 layer에 결재 관련 컴포넌트들 작업

--- order/src/main/kotlin/msa/order/infrastructure/order/payment/PaymentProcessorImpl.kt
+++ order/src/main/kotlin/msa/order/infrastructure/order/payment/PaymentProcessorImpl.kt
@@ -1,11 +1,15 @@
 package msa.order.infrastructure.order.payment
 
+import msa.order.domain.order.Order
+import msa.order.domain.order.OrderCommand
 import msa.order.domain.order.payment.PaymentProcessor
 import org.springframework.stereotype.Component
 
 @Component
-class PaymentProcessorImpl : PaymentProcessor {
-    override fun pay() {
+class PaymentProcessorImpl(
+    val paymentApiCallerList: List<PaymentApiCaller>
+) : PaymentProcessor {
+    override fun pay(order: Order, command: OrderCommand.PaymentRequest) {
         TODO("Not yet implemented")
     }
 }
