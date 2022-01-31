5934e4fac0515c8a950cba0aef4b0d03547e4dd8
sinkyoungdeok <tlsrudejr123@nate.com>
2022-01-31 21:46:39 +0900

    [FIX] (Gift) Order서비스 -> 선물하기 주문 결제 처리 API - 저장처리 안되던부분 수정

--- order/src/main/kotlin/msa/order/domain/order/gift/GiftOrderServiceImpl.kt
+++ order/src/main/kotlin/msa/order/domain/order/gift/GiftOrderServiceImpl.kt
@@ -4,6 +4,7 @@ import msa.order.common.exception.IllegalStatusException
 import msa.order.domain.order.Order
 import msa.order.domain.order.OrderCommand
 import msa.order.domain.order.OrderReader
+import msa.order.domain.order.OrderStore
 import msa.order.domain.order.payment.PaymentProcessor
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -12,7 +13,8 @@ import org.springframework.transaction.annotation.Transactional
 class GiftOrderServiceImpl(
     val orderReader: OrderReader,
     val paymentProcessor: PaymentProcessor,
-    val giftMessageChannelSender: GiftMessageChannelSender
+    val giftMessageChannelSender: GiftMessageChannelSender,
+    val orderStore: OrderStore
 ) : GiftOrderService {
     @Transactional
     override suspend fun paymentOrder(command: OrderCommand.PaymentRequest) {
@@ -22,7 +24,7 @@ class GiftOrderServiceImpl(
 
         paymentProcessor.pay(order, command)
         order.orderComplete()
-
+        orderStore.store(order)
         giftMessageChannelSender
             .paymentComplete(GiftPaymentCompleteMessage(order.orderToken))
     }
