c731d7782d2a1fe0093751237b525dfe02fe4897
sinkyoungdeok <tlsrudejr123@nate.com>
2022-03-05 12:02:39 +0900

    [ADD] (User, Admin) Order, Gift 서비스 -> 어드민용 다른 유저 정보 조회 API

--- gift/src/main/kotlin/msa/gift/interfaces/admin/AdminApiController.kt
+++ gift/src/main/kotlin/msa/gift/interfaces/admin/AdminApiController.kt
@@ -1,19 +1,18 @@
 package msa.gift.interfaces.admin
 
 import msa.gift.application.admin.AdminFacade
+import msa.gift.application.user.UserFacade
 import msa.gift.common.response.CommonResponse
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api/v1/admin")
 class AdminApiController(
     val adminDtoMapper: AdminDtoMapper,
-    val adminFacade: AdminFacade
+    val adminFacade: AdminFacade,
+    val userFacade: UserFacade
 ) {
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
@@ -22,6 +21,16 @@ class AdminApiController(
     ): CommonResponse<AdminDto.RegisterAdminResponse> {
         val command = adminDtoMapper.of(request)
         val userInfo = adminFacade.registerAdmin(command)
+        val response = adminDtoMapper.of(userInfo)
+        return CommonResponse(response)
+    }
+
+    @GetMapping("/{username}")
+    @PreAuthorize("hasRole('ADMIN')")
+    suspend fun retrieveUser(
+        @PathVariable username: String
+    ): CommonResponse<AdminDto.RegisterAdminResponse> {
+        val userInfo = userFacade.retrieveUser(username)
         val response = adminDtoMapper.of(userInfo)
         return CommonResponse(response)
     }
