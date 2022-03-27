34b1d8e9e59ca7697d07c91869ffdaa89c20b0b4
sinkyoungdeok <tlsrudejr123@nate.com>
2022-03-28 01:41:29 +0900

    {#107} [ADD] (Auth) Gift서비스 -> 리프래시 토큰을 redis에 저장해서 처리한다.

--- gift/src/main/kotlin/msa/gift/common/config/RedisRepositoryConfig.kt
+++ gift/src/main/kotlin/msa/gift/common/config/RedisRepositoryConfig.kt
@@ -1,4 +1,4 @@
-package msa.order.common.config
+package msa.gift.common.config
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -8,7 +8,6 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 import org.springframework.data.redis.serializer.StringRedisSerializer
-
 
 @Configuration
 @EnableRedisRepositories
